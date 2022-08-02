fe49b302aeead7165b5e9b3ccde29035e2e5d12d
jnfire <mesafilms@gmail.com>
2022-08-02 09:06:45 +0200

    fix website
    fix website

--- apps/website/urls.py
+++ apps/website/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from apps.web.views import home
+from apps.website.views import home
 
 urlpatterns = [
     path("", home, name="home"),
--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("apps.web.urls")),
+    path("", include("apps.website.urls")),
     path("admin/", admin.site.urls),
 ]
